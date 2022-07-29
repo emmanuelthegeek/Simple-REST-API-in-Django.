ca8fa07b558a06d2bad17fa0da3b62daabbe9686
Emmanuel Dada <altshiftctrl6@gmail.com>
2022-07-29 14:52:32 +0100

    added the rest_framework app in the installed app list

--- simpleRestAPI/settings.py
+++ simpleRestAPI/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'basicAPI',
+    'rest_framework',
+    'basicAPI'
 ]
 
 MIDDLEWARE = [
