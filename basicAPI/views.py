09a8b82d3443ba8287834fe566d534685f46b857
Emmanuel Dada <altshiftctrl6@gmail.com>
2022-07-29 15:01:14 +0100

    created all the dependencies in the views.py file

--- basicAPI/views.py
+++ basicAPI/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from django.core import serializers
+from django.conf import settings
+import json
 
 # Create your views here.
