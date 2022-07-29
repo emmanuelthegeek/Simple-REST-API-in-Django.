bb76fb9433631f7e56618544584a3bc03f3d17e2
Emmanuel Dada <altshiftctrl6@gmail.com>
2022-07-29 15:16:53 +0100

    included the logic in the views.py file

--- basicAPI/views.py
+++ basicAPI/views.py
@@ -9,4 +9,12 @@ from django.core import serializers
 from django.conf import settings
 import json
 
-# Create your views here.
+@api_view(["POST"])
+def weight(height_data):
+    try:
+        body_height = json.loads(height_data.body)
+        body_weight = str(body_height * 10)
+        return JsonResponse('Body weight = ' + body_weight + " kg", safe=False)
+    except ValueError as ve:
+        return Response(ve.args[0], status.HTTP_404_BAD_REQUEST)
+
