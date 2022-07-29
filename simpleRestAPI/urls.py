8db34c3b51dac160b550c5d1ca8b264c28d6f64b
Emmanuel Dada <altshiftctrl6@gmail.com>
2022-07-29 15:21:16 +0100

    included the urls.py dependencies

--- simpleRestAPI/urls.py
+++ simpleRestAPI/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from basicAPI import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
